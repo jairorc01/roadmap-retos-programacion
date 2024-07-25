84a9e27200071bee03a9d672cc80833d979b94fd
jairorc01 <jairoramirezcasas@gmail.com>
2024-07-25 00:49:47 -0500

    Create jairorc01.js

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/jairorc01.js
@@ -0,0 +1,27 @@
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+//Comentario en una linea
+
+/*
+Comentarios en
+varias lineas
+*/
+
+var variable = "Mi primera variable"
+var variable = "Nuevo valor en la variable"
+
+const constante = "Mi primera constante" //por convencion
+ 
+let int = 29
+let float = 29.8
+
+let string = "Hola"
+
+let datonull = null
+
+let noDefinido = undefined
+
+let booleano = true
+let booleano1 = false
+
+console.log('Hola JavaScript')
